84461c0dbecdda99a94e76df968b8a5fbfb6c6bb
Howie21 <bryce9643@gmail.com>
2021-11-01 17:09:05 -0400

    Added functionality to index.html for employee to send the days pickups to the page through context

--- Trash_Collector/employees/views.py
+++ Trash_Collector/employees/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.apps import apps
 from datetime import date
+import calendar
 from django.core.exceptions import ObjectDoesNotExist
 from .models import Employee
 from django.contrib.auth.decorators import login_required
@@ -18,20 +19,24 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     logged_in_user = request.user
+    weekdays = list(calendar.day_name)
 
     try:
         # This line will return the customer record of the logged-in user if one exists
         logged_in_employee = Employee.objects.get(user=logged_in_user)
         content_present = True
         today = date.today()
+        weekday = weekdays[date.today().weekday()]
         customer_data = Customer.objects.filter(zip_code = logged_in_employee.zip_code)
+        # current_day_pickup = Customer.objects.filter(weekly_pickup = today)
         if len(list(customer_data)) == 0:
             content_present = False
         context = {
             'logged_in_employee': logged_in_employee,
             'today': today,
             'customer_data': customer_data,
-            'content_present': content_present
+            'content_present': content_present,
+            'weekday': weekday,
         }
         return render(request, 'employees/index.html', context)
     except ObjectDoesNotExist:
