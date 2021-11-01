7a2d1bd0b906b13fd7d8fc5aafe3dbc9949447e9
Howie21 <bryce9643@gmail.com>
2021-11-01 17:44:24 -0400

    Added path for Button pathing to views.py confirm() method

--- Trash_Collector/employees/urls.py
+++ Trash_Collector/employees/urls.py
@@ -7,5 +7,6 @@ app_name = "employees"
 urlpatterns = [
     path('', views.index, name="index"),
     path('create/', views.create, name='create'),
-    path('edit_profile/', views.edit_profile, name='edit_profile')
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('<int:customer_id>/confirm/', views.confirm, name='confirm')
 ]
