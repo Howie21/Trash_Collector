8837f78f1a174245081b6cfa05bcc237d4e60b73
RaHughes <rogerallenhughes@hotmail.com>
2021-11-01 13:45:00 -0500

    Add more functionality to Index, Add create method/functionality

--- Trash_Collector/employees/views.py
+++ Trash_Collector/employees/views.py
@@ -1,13 +1,45 @@
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.apps import apps
+from datetime import date
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Employee
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
+@login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
-    return render(request, 'employees/index.html')
+    logged_in_employee = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_employee)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html', context)
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
+
+@login_required
+def create(request):
+    logged_in_user = request.user 
+    if request.method == 'POST':
+        name_from_form = request.POST.get('name')
+        zip_from_form = request.POST.get('zip_code')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, zip_code=zip_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
