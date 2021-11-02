6fbd14aa89586da524600ee631443ad837c8d6bc
RaHughes <rogerallenhughes@hotmail.com>
2021-11-02 14:20:30 -0500

    Add path for filter page

--- Trash_Collector/employees/urls.py
+++ Trash_Collector/employees/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('<int:customer_id>/confirm/', views.confirm, name='confirm'),
-    path('filter/', views.filter, name='filter'),
-    # path('<str:date_select>/filterp/', views.filterp, name='filterp')
+    path('filter/', views.filter, name='filter')
 ]
